8c0e131c3cafde1ef2cd2957d1bd8f12f6411387
Luke Berry <lukeberrypi@gmail.com>
2023-02-23 00:52:08 -0300

    [chore]: add karla font-family

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>Hello world</h1>
+  return (
+    <main className="bg-zinc-900 w-screen h-screen">
+      <h1 className="text-5xl text-white font-bold">&#x0278;</h1> 
+      <h1 className="text-5xl text-white"><strong>phive</strong> at a time</h1>
+    </main>
+  )
 }
 
 export default App
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,10 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Karla', 'sans-serif'],
+      heading: ['Karla', 'sans-serif'],
+    }
   },
   plugins: [],
 }
