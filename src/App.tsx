c1f5ce6710bfed49b5ad7d4612216c9fb2b482a9
Luke Berry <lukeberrypi@gmail.com>
2023-03-16 16:21:05 -0300

    [fix]: twitter share banner

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ export default function App() {
   useEffect(() => {
     const icon = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
     const title = document.querySelector("title") as HTMLTitleElement;
-
     if (tasks.some((task) => !!task)) {
       icon.href = "/favicon-alert.png";
       title.innerText = `[${tasks.filter((task) => !!task).length}] phived`;
@@ -25,9 +24,6 @@ export default function App() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center bg-lightWhite selection:bg-berryBlue dark:bg-darkerBlack dark:selection:bg-purpleRain">
       <Header clearTasks={clearTasks} />
-      <span aria-hidden="true" className="top-16 mb-4 block text-xs dark:text-lightWhite md:hidden">
-        🚧 touch functionality under work 🚧
-      </span>
       <Tasks tasks={tasks} setTasks={setTasks} />
       <Logo />
       <Footer />
