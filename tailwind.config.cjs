e82c7e69425f10f84111d7899d9715e7e4770cdb
Luke Berry <lukeberrypi@gmail.com>
2023-03-17 15:18:44 -0300

    [fix]: adjust responsiveness

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
+      tiny: "400px",
       xs: "460px",
       ...defaultTheme.screens,
     },
