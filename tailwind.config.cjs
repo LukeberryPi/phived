95fc68277517325c53a6643d396fee092256a7c0
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2023-03-18 16:00:47 -0300

    chore(TailwindConfig): add boxShadow variants on tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,7 +22,12 @@ module.exports = {
       instagramPurple: "#962fbf",
       instagramOrange: "#fa7e1e",
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        ['brutalist-dark']: '3px 3px #080811',
+        ['brutalist-light']: '2px 2px #f1f2f3',
+      }
+    },
     fontFamily: {
       sans: ["DM Sans", "sans-serif"],
       heading: ["DM Sans", "sans-serif"],
