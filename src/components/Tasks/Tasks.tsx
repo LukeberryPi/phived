9f55af40ba6e510035a2196721ea6da24d6bcf37
LukeberryPi <lukeberrypi@gmail.com>
2023-03-19 12:18:49 -0300

    Revert "refactor: remove mult index and external type file"

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,10 +1,6 @@
-import { useEffect, useState, Dispatch } from "react";
-import { placeholders } from "src/content/contents";
-
-export type TasksProps = {
-  tasks: string[];
-  setTasks: Dispatch<string[]>;
-};
+import { useEffect, useState } from "react";
+import { placeholders } from "src/content";
+import { TasksProps } from "./Tasks.types";
 
 export function Tasks({ tasks, setTasks }: TasksProps) {
   const [placeholder, setPlaceholder] = useState<string>("");
