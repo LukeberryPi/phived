c2619a45a9849056f3a84806bc59458d2a9b89cb
João Victor <santosjoao.dev@gmail.com>
2023-03-20 20:36:14 -0300

    [refactor]: improve code quality and readability

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -1,25 +1,20 @@
 import { Helmet } from "react-helmet-async";
-import { useEffect, useState } from "react";
 import type { HeadProps } from "src/components/Head/Head.types";
 
-export const Head = ({ tasks }: HeadProps) => {
-  const [title, setTitle] = useState("phived");
-  const [icon, setIcon] = useState("/favicon-default.ico");
+const ALERT_ICON = "/favicon-alert.ico"
+const DEFAULT_ICON = "/favicon-default.ico"
 
-  useEffect(() => {
-    if (tasks.some((task) => !!task)) {
-      setIcon("/favicon-alert.ico");
-      setTitle(`[${tasks.filter((task) => !!task).length}] phived - the anti-procrastination to-do list`);
-    } else {
-      setIcon("/favicon-default.ico");
-      setTitle("phived - the anti-procrastination to-do list");
-    }
-  }, [tasks]);
+export const Head = ({ tasks }: HeadProps) => {
+  const enableTasks = tasks.filter((task) => Boolean(task))
+  const titlePrefix= enableTasks.length ? `[${enableTasks.length}]` : undefined
+  const defaultTitle = "phived - the anti-procrastination to-do list"
 
   return (
     <Helmet>
-      <title>{title}</title>
-      <link rel="icon" type="image/x-icon" href={icon} />
+      <title>{
+        [titlePrefix, defaultTitle].join(' ')
+      }</title>
+      <link rel="icon" type="image/x-icon" href={enableTasks.length ? ALERT_ICON : DEFAULT_ICON} />
     </Helmet>
   );
 };
