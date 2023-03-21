7df5ae2bac919248d4a1d5fcbc82579dcb78d78e
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2023-03-21 13:02:19 -0300

    [refactor]: apply suggestions
    - turn "titlePrefix" on "BASE_PAGE_TITLE"
    - remove unnecessary array instead of template string

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -1,22 +1,25 @@
 import { Helmet } from "react-helmet-async";
-import type { HeadProps } from "src/components/Head/Head.types";
 import { useTasksContext } from "src/contexts";
 
-const ALERT_ICON = "/favicon-alert.ico"
-const DEFAULT_ICON = "/favicon-default.ico"
+const ALERT_ICON = "favicon-alert.ico"
+const DEFAULT_ICON = "favicon-default.ico"
+const BASE_PAGE_TITLE = "phived - the anti-procrastination to-do list"
 
 export const Head = () => {
   const { tasks } = useTasksContext();
   const enableTasks = tasks.filter(Boolean)
-  const titlePrefix= enableTasks.length ? `[${enableTasks.length}]` : undefined
-  const defaultTitle = "phived - the anti-procrastination to-do list"
+  const titlePrefix= enableTasks.length ? `[${enableTasks.length}]` : ''
 
   return (
     <Helmet>
       <title>{
-        [titlePrefix, defaultTitle].join(' ')
+        `${titlePrefix} ${BASE_PAGE_TITLE}`
       }</title>
-      <link rel="icon" type="image/x-icon" href={enableTasks.length ? ALERT_ICON : DEFAULT_ICON} />
+      <link 
+        rel="icon" 
+        type="image/x-icon" 
+        href={`/${enableTasks.length ? ALERT_ICON : DEFAULT_ICON}`} 
+      />
     </Helmet>
   );
 };
