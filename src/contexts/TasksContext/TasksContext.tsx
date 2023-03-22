00fb3bf19b35e37a7e724954dc28c0ada292e20c
Nick Gabe <nicolasgabrielctt@gmail.com>
2023-03-21 23:16:09 -0300

    lint files to match eslint prettier plugin

--- src/contexts/TasksContext/TasksContext.tsx
+++ src/contexts/TasksContext/TasksContext.tsx
@@ -2,7 +2,7 @@ import { createContext, useContext } from "react";
 import type { TaskContextType } from "src/contexts/TasksContext/TasksContext.types";
 
 export const initialState = {
-  tasks: Array<string>(5).fill("")
+  tasks: Array<string>(5).fill(""),
 } as TaskContextType;
 
 export const TasksContext = createContext(initialState);
