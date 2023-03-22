401a47e86378ddb7e25568a4235ef0628a0fed50
Richard Bidin <richardbidin@outlook.com>
2023-03-22 14:01:46 -0300

    fix: Merge commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,6 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-<<<<<<< HEAD
-=======
-import path from "path";
->>>>>>> 6c2fd9c5196e1304854c719f0fd293b0ca8b5b2b
 
 export default defineConfig({
   plugins: [
