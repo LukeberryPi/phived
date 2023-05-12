6e61b533d1d3472f673118a2b92e40ccdc6d8ea0
Luke Berry <luke.berry@newfold..com>
2023-05-12 17:09:15 -0300

    chore: add icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,1 +1,4 @@
 export { DragIcon } from "src/icons/DragIcon";
+export { DarkModeIcon } from "src/icons/DarkModeIcon";
+export { LightModeIcon } from "src/icons/LightModeIcon";
+export { HelpIcon } from "src/icons/HelpIcon";
