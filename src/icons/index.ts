d65b66c7bd94ce670cba33d0dbef0403d055e524
datsfilipe <datsfilipe.foss@proton.me>
2023-06-13 17:25:07 -0300

    refactor: remove ArrowDownIcon icon

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -3,7 +3,6 @@ export { DarkModeIcon } from "src/icons/DarkModeIcon";
 export { LightModeIcon } from "src/icons/LightModeIcon";
 export { ClearTasksIcon } from "src/icons/ClearTasksIcon";
 export { ArrowUpIcon } from "src/icons/ArrowUpIcon";
-export { ArrowDownIcon } from "src/icons/ArrowDownIcon";
 export { InstagramIcon } from "src/icons/InstagramIcon";
 export { GithubIcon } from "src/icons/GithubIcon";
 export { TwitterIcon } from "src/icons/TwitterIcon";
