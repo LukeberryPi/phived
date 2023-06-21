7e6701d6d2230b444fac6ca836146ab5d8b20e2f
Luke Berry <lukeberrypi@gmail.com>
2023-06-20 21:56:30 -0300

    addAnimationToLogo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,9 @@
 export function Logo() {
   return (
-    <a href="/" className="cursor-pointer text-4xl font-bold text-softBlack dark:text-softWhite">
+    <a
+      href="/"
+      className="cursor-pointer text-4xl font-bold text-softBlack transition-transform active:scale-95 dark:text-softWhite"
+    >
       phived
     </a>
   );
