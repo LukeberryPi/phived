58306e9e2c866807918806fa3136ae318a825921
Luke Berry <lukeberrypi@gmail.com>
2023-09-12 23:36:28 -0300

    revert fuchsia, cache fixed!

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Footer, Header, Message, Tasks, Head } from "src/components";
 
 export default function App() {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center bg-fuchsia-500 selection:bg-berryBlue dark:bg-trueBlack dark:selection:bg-purpleRain">
+    <div className="flex h-full w-full flex-col items-center justify-center bg-softWhite selection:bg-berryBlue dark:bg-trueBlack dark:selection:bg-purpleRain">
       <HelmetProvider>
         <Head />
       </HelmetProvider>
