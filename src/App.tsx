c3a23a34167478a46cd5bca54e08ff116c640d85
Luke Berry <lukeberrypi@gmail.com>
2023-10-20 17:33:58 -0300

    degrade react router dom to 5.3.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import { HelmetProvider } from "react-helmet-async";
-import { BrowserRouter } from "react-router-dom";
-import { Footer, Header, Message, Tasks, Head } from "src/components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "src/components";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className="flex h-full w-full flex-col items-center justify-center bg-softWhite selection:bg-berryBlue dark:bg-trueBlack dark:selection:bg-purpleRain">
-        <HelmetProvider>
-          <Head />
-        </HelmetProvider>
-        <Header />
-        <Tasks />
-        <Message />
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,4 @@ export { Head } from "src/components/Head";
 export { Message } from "src/components/Message";
 export { HelpMenu } from "src/components/HelpMenu";
 export { ModeSelector } from "src/components/ModeSelector";
+export { Home } from "src/components/Home";
