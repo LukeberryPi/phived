9861ec2ea36710fa532d838adcaab054464b2e23
Luke Berry <lukeberrypi@gmail.com>
2023-10-25 00:25:23 -0300

    remove unused variable

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,5 @@
 /* eslint-disable */
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
