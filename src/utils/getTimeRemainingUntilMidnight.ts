c45d85f14ea742e3be73855c852944a1ab9220be
Luke Berry <lukeberrypi@gmail.com>
2023-12-25 20:43:34 -0300

    add midnight util

---
+++ src/utils/getTimeRemainingUntilMidnight.ts
@@ -0,0 +1,17 @@
+function getTimeRemainingUntilMidnight() {
+  const now = new Date();
+  const midnight = new Date();
+  midnight.setHours(24, 0, 0, 0);
+
+  const timeRemaining = midnight.getTime() - now.getTime();
+
+  const hours = Math.floor((timeRemaining / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((timeRemaining / 1000 / 60) % 60);
+  const seconds = Math.floor((timeRemaining / 1000) % 60);
+
+  return {
+    hours: String(hours).padStart(2, "0"),
+    minutes: String(minutes).padStart(2, "0"),
+    seconds: String(seconds).padStart(2, "0"),
+  };
+}
