fd211bf276263494bf63e63a9324808bffb23bbc
Luke Berry <lukeberrypi@gmail.com>
2024-01-07 00:49:46 -0300

    remove debug only reset everything button

--- src/components/DailyTasks.tsx
+++ src/components/DailyTasks.tsx
@@ -227,14 +227,6 @@ export function DailyTasks() {
         <p className="mx-auto w-fit rounded-lg bg-dailyGreen px-2 py-1 text-sm dark:bg-dailyPurple dark:text-trueWhite sm:text-base">
           daily
         </p>
-        <button
-          onClick={() => {
-            setDailyTasks(["", "", "", "", ""]);
-            setDailyTasksLastDoneAt([]);
-          }}
-        >
-          reset everything
-        </button>
         <p className="text-lg text-trueBlack dark:text-trueWhite xs:text-xl sm:text-2xl">
           what do you want to do?
         </p>
