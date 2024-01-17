b122275612a073490ef6d3955f9911c30fde69ec
Luke Berry <lukeberry@Lukes-MacBook-Pro.local>
2024-01-17 19:22:26 -0300

    adjust styles

--- src/utils/handleSetTheme.ts
+++ src/utils/handleSetTheme.ts
@@ -2,8 +2,11 @@ export function handleSetTheme(isDarkMode: boolean) {
   if (isDarkMode) {
     document.documentElement.classList.add("dark");
     localStorage.setItem("theme", "dark");
-  } else {
+    return;
+  }
+  if (!isDarkMode) {
     document.documentElement.classList.remove("dark");
     localStorage.setItem("theme", "light");
+    return;
   }
 }
