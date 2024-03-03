feaada7f4110fafff091a5d7c42d435e2dc0eade
Luke Berry <lukeberrypi@gmail.com>
2024-03-03 00:11:01 -0300

    adjust prettier config

--- src/utils/isThemeSetToDark.ts
+++ src/utils/isThemeSetToDark.ts
@@ -1,5 +1,8 @@
 export function isThemeSetToDark() {
-  const localStorageTheme = localStorage.getItem("theme")
-  const isBrowserDark = window && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-  return !!localStorageTheme ? localStorageTheme === 'dark' : isBrowserDark
+  const localStorageTheme = localStorage.getItem("theme");
+  const isBrowserDark =
+    window &&
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  return !!localStorageTheme ? localStorageTheme === "dark" : isBrowserDark;
 }
