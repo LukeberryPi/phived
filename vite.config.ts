4e59a5d61967caa3d4eff526324d33852a06204d
Luke Berry <lukeberrypi@gmail.com>
2024-03-11 12:09:52 -0300

    Revert "remove everything pwa related"
    This reverts commit 9a5a74824b618b1d6ad2091d08cc42e8909e3dd9.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: "auto",
+      registerType: "prompt",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      src: path.resolve("src/"),
+    },
+  },
 });
