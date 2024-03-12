ebf2f45bd311df7352289fbbaca54026254df9c2
Luke Berry <lukeberrypi@gmail.com>
2024-03-12 13:12:49 -0300

    .

--- src/icons/Open.tsx
+++ src/icons/Open.tsx
@@ -12,7 +12,7 @@ export function Open({ className, size = 24 }: DefaultSvgProps) {
       <path
         fill="currentColor"
         d="M224 104a8 8 0 0 1-16 0V59.32l-66.33 66.34a8 8 0 0 1-11.32-11.32L196.68 48H152a8 8 0 0 1 0-16h64a8 8 0 0 1 8 8Zm-40 24a8 8 0 0 0-8 8v72H48V80h72a8 8 0 0 0 0-16H48a16 16 0 0 0-16 16v128a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-72a8 8 0 0 0-8-8"
-      ></path>
+      />
     </svg>
   );
 }
